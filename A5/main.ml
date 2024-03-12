466b89695efc8fd08acb651553001b86592bd619
rogerthat1729 <cs1221603@iitd.ac.in>
2024-03-12 21:46:29 +0530

    added few tests

--- A5/main.ml
+++ A5/main.ml
@@ -9,14 +9,13 @@ type substitution = (string * tree) list
 let rec check_sig s = 
   match s with
   | [] -> true
-  | (a, b)::t -> (b>=0) && (check_sig t) && not(List.mem (a, b) t);;
+  | (a, b)::t -> (b>=0) && not(List.mem (a, b) t) && (check_sig t);;
 ;;
 
 let rec wftree t = 
   match t with
   | V _ -> true
-  | C {node = (a, b); children = l} ->
-    if b == List.length l then List.fold_left (&&) true (List.map wftree l) else false
+  | C {node = (a, b); children = l} -> (b = List.length l) && (List.fold_left (&&) true (List.map wftree l))
 ;;
   
 let rec ht t =
@@ -53,6 +52,15 @@ let rec mirror t =
   | C {node = (a, b); children = l} -> C {node = (a, b); children = List.rev (List.map mirror l)}
 ;;
 
+(* To check if a table is a valid representation of a function *)
+  (* -> There can't be two tuples in the table with same first element - even if the second element is the same, as this is redundant *)
+
+let rec check_table g = 
+  match g with
+  | [] -> true
+  | (a, b)::t -> not(List.mem_assoc a t) && (check_table t)
+;;
+
 let rec subst s t =
   match t with
   | V x -> s (V x)
@@ -74,8 +82,8 @@ let mgu t1 t2 =
   let rec mgu_function t1 t2 t =
     match t1, t2 with
     | V x, V y -> subst (subst_helper [(x, V y)]) t
-    | V x, C r | C r, V x -> if (List.mem x (vars (C r))) then failwith "NOT_UNIFIABLE" else subst (subst_helper [(x, C r)]) t
-    | C r1, C r2 -> if r1.node <> r2.node then failwith "NOT_UNIFIABLE" 
+    | V x, C r | C r, V x -> if (List.mem x (vars (C r))) then failwith "NOT_UNIFABLE - variable repeat" else subst (subst_helper [(x, C r)]) t
+    | C r1, C r2 -> if r1.node <> r2.node then failwith "NOT_UNIFABLE - different nodes" 
     else 
     (
       let rec recursive_mgu_fn idx l1 l2 t =
@@ -124,11 +132,16 @@ let rec print_tree indent t =
     print_chars " " indent; print_string "|"; print_string "-"; print_string a; print_newline();
     List.iter (fun x -> print_tree (indent+2) x) l
   ;;
+
+let print_vars l = 
+  match l with
+  | [] -> print_string "[]"
+  | _ -> print_string "vars: ["; List.iter (fun x -> print_string x; print_string ";") l; print_string "]\n"
   
 let print_list l = 
   match l with
   | [] -> print_string "[]"
-  | _ -> List.iter (fun (a, b) -> print_string a; print_string ":\n"; print_tree 0 b) l
+  | _ -> print_string "[\n"; List.iter (fun (a, b) ->print_string " "; print_string a; print_string ":\n"; print_tree 0 b) l; print_string "\n]\n"
 ;;
 
 (* let t3 = V "x";;
@@ -138,18 +151,12 @@ let t2 = C {node = ("c", 1); children = [t1]};;("a", t2); ("b", t1);
 let t5 = C {node = ("a", 2); children = [V "p"; V "q"]};;
 let t = C {node = ("a", 2) ; children = [t1;t2]};; *)
 
-let a = C {node = ("a", 0) ; children = []} ;;
+(* let a = C {node = ("a", 0) ; children = []} ;;
 let b = C {node = ("b", 0) ; children = []} ;;
-let haX = C {node = ("h", 2) ; children = [a ; V "x"]} ;;
-let hXb = C {node = ("h", 2) ; children = [ V "x" ; b]} ;;
-let t1 = C {node = ("f", 2) ; children = [haX ; hXb]} ;;
-let hab = C {node = ("h", 2) ; children = [a ; b]} ;;
-let hZX = C {node = ("h", 2) ; children = [V "z" ; V "x" ]} ;;
-let t2 = C {node = ("f", 2) ; children = [hab ; hZX]} ;;
-let t3 = C {node = ("f", 2); children = [haX; hXb]}
+
 let s2 = C {node = ("b", 0); children = []};;
 let s1 = C {node = ("a", 2); children = [s2; V "x"]};;
-let p1 = C {node = ("a", 2); children = [V "x"; V "z"]};;
+let p1 = C {node = ("a", 2); children = [V "x"; V "z"]};; *)
   
 (* print_endline (string_of_int (ht t3)) *)
 (* print_endline (string_of_bool (wftree t)) *)
@@ -169,3 +176,66 @@ let p1 = C {node = ("a", 2); children = [V "x"; V "z"]};;
 
 (* Write test cases for mgu and other functions *)
 
+(* Testcases *)
+
+(* check_sig *)
+ let s = [("a", 0); ("b", 2); ("c", 1); ("a",1)];;
+(* print_endline (string_of_bool (check_sig s));;  *)
+
+(* wftree *)
+ let t1 = C {node = ("a", 2); children = [V "x"; C {node = ("m", 0); children = [C {node = ("n", 0); children = []}]}]};;
+(* print_endline (string_of_bool (wftree t1));;  *)
+
+(* ht, size *)
+let t2 = C {node = ("a", 0); children = []};;
+let t3 = C {node = ("b", 2); children = [t2; t2]};;
+let t4 = C {node = ("c", 2); children = [t2; t3]};;
+(* print_endline (string_of_int (ht t4));; *)
+(* print_endline (string_of_int (size t4));;  *)
+
+(* vars *)
+let t5 = C {node = ("b", 2); children = [V "y"; t2]};;
+let t6 = C {node = ("c", 2); children = [V "x"; t5]};;
+let t7 = C {node = ("a", 2); children = [t6; V "y"]};;
+(* print_vars(vars t7);;  *)
+
+(* mirror *)
+(* print_tree 0 (t7);; *)
+(* print_tree 0 (mirror t7);; *)
+
+(* check_table *)
+(* print_string(string_of_bool (check_table [("a", t1); ("b", t2); ("c", t3)])); print_string "\n";; *)
+(* print_string(string_of_bool (check_table [("x", t4); ("y", t5); ("x", t6)])); print_string "\n";; *)
+
+(* composition of substitutions *)
+let sigma1 = [("x", t3); ("y", t4); ("z", V "w")];;
+let sigma2 = [("w", t2); ("x", t1)];;
+(* print_list sigma1;; *)
+(* print_list sigma2;; *)
+(* print_list(compose_subst sigma1 sigma2);; *)
+
+(* mgu *)
+
+let t8 = C {node = ("b", 0); children = []};;
+let t9 = C {node = ("d", 3); children = [V "x"; V "y"; t2]};;
+let t10 = C {node = ("d", 3); children = [t8; V "z"; V "z"]};;
+
+
+let haX = C {node = ("h", 2) ; children = [t2 ; V "x"]} ;;
+let hXb = C {node = ("h", 2) ; children = [ V "x" ; t8]} ;;
+let t1 = C {node = ("f", 2) ; children = [haX ; hXb]} ;;
+let hab = C {node = ("h", 2) ; children = [t2 ; t8]} ;;
+let hZX = C {node = ("h", 2) ; children = [V "z" ; V "x" ]} ;;
+let t11 = C {node = ("f", 2) ; children = [hab ; hZX]} ;;
+let t12 = C {node = ("f", 2); children = [haX; hXb]} ;;
+
+let t13 = C {node = ("a", 2); children = [V "x"; V "y"]};;
+let t14 = C {node = ("b", 2); children = [V "x"; V "x"]};;
+
+let t15 = V "j";;
+let t16 = C {node = ("a", 2); children = [V "x"; V "j"]};;
+
+(* print_list (mgu t9 t10);; *)
+(* print_list (mgu t11 t12) ;; *)
+(* print_list (mgu t13 t14) ;; *)
+(* print_list (mgu t15 t16);; *)
