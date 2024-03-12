b29693899b8985d117bbc1fc0eef2e50c0fb4d83
rogerthat1729 <cs1221603@iitd.ac.in>
2024-03-12 22:28:39 +0530

    final code

--- A5/main.ml
+++ A5/main.ml
@@ -78,6 +78,12 @@ let compose_subst sigma1 sigma2 =
   List.map (fun x -> (x, subst (subst_helper sigma2) (subst (subst_helper sigma1) (V x)))) all_vars
 ;;
 
+let rec tree_eq t1 t2 = 
+  match t1, t2 with
+  | V x, V y -> x = y
+  | C r1, C r2 -> r1.node = r2.node && (List.fold_left (&&) true (List.map2 tree_eq r1.children r2.children))
+  | C r, V x | V x, C r -> false
+
 let mgu t1 t2 =
   let rec mgu_function t1 t2 t =
     match t1, t2 with
@@ -99,8 +105,13 @@ let mgu t1 t2 =
     )
       in
   let variables = union (vars t1) (vars t2) in
+  match variables with
+  | [] -> if (tree_eq t1 t2) then [] else failwith "NOT_UNIFABLE - different nodes"
+  | _ -> 
+    (
   let init_list = List.map (fun x -> (x, mgu_function t1 t2 (V x))) variables in
   List.filter (fun (a, b) -> b <> (V a)) init_list
+    )
 ;;
   
 
@@ -141,40 +152,8 @@ let print_vars l =
 let print_list l = 
   match l with
   | [] -> print_string "[]"
-  | _ -> print_string "[\n"; List.iter (fun (a, b) ->print_string " "; print_string a; print_string ":\n"; print_tree 0 b) l; print_string "\n]\n"
+  | _ -> print_string "Begin\n"; List.iter (fun (a, b) -> print_string a; print_string ":\n"; print_tree 0 b) l; print_string "End\n"
 ;;
-
-(* let t3 = V "x";;
-let t4 = V "y";;  
-let t1 = C {node = ("b", 0); children = []};;
-let t2 = C {node = ("c", 1); children = [t1]};;("a", t2); ("b", t1);
-let t5 = C {node = ("a", 2); children = [V "p"; V "q"]};;
-let t = C {node = ("a", 2) ; children = [t1;t2]};; *)
-
-(* let a = C {node = ("a", 0) ; children = []} ;;
-let b = C {node = ("b", 0) ; children = []} ;;
-
-let s2 = C {node = ("b", 0); children = []};;
-let s1 = C {node = ("a", 2); children = [s2; V "x"]};;
-let p1 = C {node = ("a", 2); children = [V "x"; V "z"]};; *)
-  
-(* print_endline (string_of_int (ht t3)) *)
-(* print_endline (string_of_bool (wftree t)) *)
-(* print_tree 0 t;; *)
-(* print_tree 0 (mirror t);; *)
-(* print_tree 0 (subst [("x", t4)] t);; *)
-(* print_tree 0 (subst [(t4, t3)] (subst [(t3, t4)] t));; *)
-(* print_tree 0 (compose_subst [(t3, t4)] [(t4, t1)] t);; *)
-(* print_list([("x", t3); ("y", t4)]);; *)
-(* print_list([("a", t1); ("b", t2)]);; *)
-(* print_list(compose [("x", (V "z"))] [("z", a)]);; *)
-(* print_list (mgu t2 t3);; *)
-(* print_list (mgu hXb hZX);; *)
-(* print_list (mgu (V "x") (V "z")) *)
-(* print_list (mgu (mirror t) (mirror t5));; *)
-(* print_string (string_of_bool (list_equal (mgu t5 t) (mgu (mirror t) (mirror t5))));; *)
-
-(* Write test cases for mgu and other functions *)
 
 (* Testcases *)
 
@@ -182,9 +161,11 @@ let p1 = C {node = ("a", 2); children = [V "x"; V "z"]};; *)
  let s = [("a", 0); ("b", 2); ("c", 1); ("a",1)];;
 (* print_endline (string_of_bool (check_sig s));;  *)
 
+
 (* wftree *)
  let t1 = C {node = ("a", 2); children = [V "x"; C {node = ("m", 0); children = [C {node = ("n", 0); children = []}]}]};;
 (* print_endline (string_of_bool (wftree t1));;  *)
+
 
 (* ht, size *)
 let t2 = C {node = ("a", 0); children = []};;
@@ -193,26 +174,41 @@ let t4 = C {node = ("c", 2); children = [t2; t3]};;
 (* print_endline (string_of_int (ht t4));; *)
 (* print_endline (string_of_int (size t4));;  *)
 
+
 (* vars *)
 let t5 = C {node = ("b", 2); children = [V "y"; t2]};;
 let t6 = C {node = ("c", 2); children = [V "x"; t5]};;
 let t7 = C {node = ("a", 2); children = [t6; V "y"]};;
 (* print_vars(vars t7);;  *)
 
+
 (* mirror *)
 (* print_tree 0 (t7);; *)
 (* print_tree 0 (mirror t7);; *)
+
 
 (* check_table *)
 (* print_string(string_of_bool (check_table [("a", t1); ("b", t2); ("c", t3)])); print_string "\n";; *)
 (* print_string(string_of_bool (check_table [("x", t4); ("y", t5); ("x", t6)])); print_string "\n";; *)
 
+
 (* composition of substitutions *)
 let sigma1 = [("x", t3); ("y", t4); ("z", V "w")];;
 let sigma2 = [("w", t2); ("x", t1)];;
+
+let sigma3 = [("x", V "y"); ("y", V "z")];;
+let sigma4 = [("z", V "x")];;
+
 (* print_list sigma1;; *)
 (* print_list sigma2;; *)
 (* print_list(compose_subst sigma1 sigma2);; *)
+(* print_list(compose_subst sigma3 sigma4);; *)
+
+
+(* subst *)
+
+(* print_tree 0 (subst (subst_helper sigma1) t7);; *)
+
 
 (* mgu *)
 
@@ -235,7 +231,32 @@ let t14 = C {node = ("b", 2); children = [V "x"; V "x"]};;
 let t15 = V "j";;
 let t16 = C {node = ("a", 2); children = [V "x"; V "j"]};;
 
+let t17 = C {node = ("g", 1); children = [V "x"]};;
+let t18 = C {node = ("g", 1); children = [V "y"]};;
+
+let t19 = C {node = ("a", 2); children = [t2; V "x"; t17]};;
+let t20 = C {node = ("a", 2); children = [t2; V "y"; t18]};;
+
+let t21 = C {node = ("w", 1); children = [V "x"]};;
+let t22 = C {node = ("w", 1); children = [V "x"]};;
+
+let t23 = C {node = ("w", 1); children = [V "y"]}
+let t24 = C {node = ("r", 2); children = [t21; V "x"]};;
+let t25 = C {node = ("r", 2); children = [t23; V "z"]};;
+
+let t26 = C {node = ("f", 2); children = [t2; t8]};;
+let t27 = C {node = ("f", 2); children = [t8; t2]};;
+
+let t28 = V "ok";;
+let t29 = C {node = ("b", 1); children = [t2]};;
+
 (* print_list (mgu t9 t10);; *)
 (* print_list (mgu t11 t12) ;; *)
 (* print_list (mgu t13 t14) ;; *)
 (* print_list (mgu t15 t16);; *)
+(* print_list (mgu t19 t20);; *)
+(* print_list (mgu t21 t22);; *)
+(* print_list (mgu t25 t24);; *)
+(* print_list (mgu t26 t27);; *)
+(* print_list (mgu t28 t29);; *)
+
