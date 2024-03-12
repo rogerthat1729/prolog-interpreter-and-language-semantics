a9ad7af0b4ccf8cf30dc9c238f0e04493ff13f02
rogerthat1729 <cs1221603@iitd.ac.in>
2024-03-12 19:17:27 +0530

    fixed mgu and compose (hopefully :))

--- A5/main.ml
+++ A5/main.ml
@@ -10,13 +10,14 @@ let rec check_sig s =
   match s with
   | [] -> true
   | (a, b)::t -> (b>=0) && (check_sig t) && not(List.mem (a, b) t);;
+;;
 
 let rec wftree t = 
   match t with
   | V _ -> true
   | C {node = (a, b); children = l} ->
     if b == List.length l then List.fold_left (&&) true (List.map wftree l) else false
-  ;;
+;;
   
 let rec ht t =
   match t with
@@ -52,41 +53,63 @@ let rec mirror t =
   | C {node = (a, b); children = l} -> C {node = (a, b); children = List.rev (List.map mirror l)}
 ;;
 
-let compose_subst sigma1 sigma2 = 
-  let sigma1_vars = List.map (fun (a, b) -> a) sigma1 in
-  let new_bindings = List.filter (fun (a, b) -> not(List.mem a sigma1_vars)) sigma2 in
-  sigma1 @ new_bindings
-;;
-
-
-let rec subst sigma t =
+let rec subst s t =
   match t with
-  | V x -> (try List.assoc x sigma with Not_found -> t)
-  | C {node = (a, b); children = l} -> C {node = (a, b); children = List.map (subst sigma) l}
+  | V x -> s (V x)
+  | C {node = (a, b); children = l} -> C {node = (a, b); children = List.map (subst s) l}
 ;;
 
-let rec mgu t1 t2 =
-  match t1, t2 with
-  | V x, V y -> if x == y then [] else [(x, V y)]
-  | V x, C r | C r, V x -> if (List.mem x (vars (C r))) then failwith "NOT_UNIFIABLE" else [(x, C r)]
-  | C r1, C r2 -> if r1.node <> r2.node then failwith "NOT_UNIFIABLE" else List.fold_left compose_subst [] (List.map2 mgu r1.children r2.children) 
+let subst_helper sigma x =
+  match x with
+  | V x -> (try List.assoc x sigma with Not_found -> V x)
+  | _ -> x
 ;;
 
-let rec subset l1 l2 =
-  match l1 with
-  | [] -> true
-  | h::t -> (List.mem h l2) && (subset t l2)
+let compose_subst sigma1 sigma2 = 
+  let all_vars = union (List.map (fun (a, b) -> a) sigma1) (List.map (fun (a, b) -> a) sigma2) in
+  List.map (fun x -> (x, subst (subst_helper sigma2) (subst (subst_helper sigma1) (V x)))) all_vars
+;;
+
+let mgu t1 t2 =
+  let rec mgu_function t1 t2 t =
+    match t1, t2 with
+    | V x, V y -> subst (subst_helper [(x, V y)]) t
+    | V x, C r | C r, V x -> if (List.mem x (vars (C r))) then failwith "NOT_UNIFIABLE" else subst (subst_helper [(x, C r)]) t
+    | C r1, C r2 -> if r1.node <> r2.node then failwith "NOT_UNIFIABLE" 
+    else 
+    (
+      let rec recursive_mgu_fn idx l1 l2 t =
+        match idx with 
+        | 0 -> subst (subst_helper []) t
+        | _ -> 
+          (
+            let sigma = recursive_mgu_fn (idx-1) l1 l2 in
+            subst (mgu_function (subst sigma (List.nth l1 (idx-1))) (subst sigma (List.nth l2 (idx-1)))) (subst sigma t)
+          )
+        in
+        recursive_mgu_fn (List.length r1.children) r1.children r2.children t
+    )
+      in
+  let variables = union (vars t1) (vars t2) in
+  let init_list = List.map (fun x -> (x, mgu_function t1 t2 (V x))) variables in
+  List.filter (fun (a, b) -> b <> (V a)) init_list
+;;
+  
+
+
+(* ----------------------------------------------------------------------------------------------------------------------------------  *)
+
+
+
 
 let list_equal l1 l2 = 
+  let rec subset l1 l2 =
+    match l1 with
+    | [] -> true
+    | h::t -> (List.mem h l2) && (subset t l2)
+  in
   (subset l1 l2) && (subset l2 l1)
 ;;
-
-(* let t3 = V "x";;
-let t4 = V "y";;  
-let t1 = C {node = ("b", 0); children = []};;
-let t2 = C {node = ("c", 1); children = [t1]};;
-let t5 = C {node = ("a", 2); children = [V "p"; V "q"]};;
-let t = C {node = ("a", 2) ; children = [t1;t2]};; *)
 
 let rec print_chars ch indent =
   match indent with 
@@ -105,17 +128,25 @@ let rec print_tree indent t =
 let print_list l = 
   match l with
   | [] -> print_string "[]"
-  | _ -> List.iter (fun (a, b) -> print_string a; print_string "->\n"; print_tree 0 b) l
+  | _ -> List.iter (fun (a, b) -> print_string a; print_string ":\n"; print_tree 0 b) l
 ;;
 
-let a = C {node = ("a",0) ; children = []} ;;
-let b = C {node = ("b",0) ; children = []} ;;
-let haX = C {node = ("h",2) ; children = [a ; V "x"]} ;;
-let hXb = C {node = ("h",2) ; children = [ V "x" ; b]} ;;
-let t1 = C {node = ("f",2) ; children = [haX ; hXb]} ;;
-let hab = C {node = ("h",2) ; children = [a ; b]} ;;
-let hZX = C {node = ("h",2) ; children = [V "z" ; V "x" ]} ;;
-let t2 = C {node = ("f",2) ; children = [hab ; hZX]} ;;
+(* let t3 = V "x";;
+let t4 = V "y";;  
+let t1 = C {node = ("b", 0); children = []};;
+let t2 = C {node = ("c", 1); children = [t1]};;("a", t2); ("b", t1);
+let t5 = C {node = ("a", 2); children = [V "p"; V "q"]};;
+let t = C {node = ("a", 2) ; children = [t1;t2]};; *)
+
+let a = C {node = ("a", 0) ; children = []} ;;
+let b = C {node = ("b", 0) ; children = []} ;;
+let haX = C {node = ("h", 2) ; children = [a ; V "x"]} ;;
+let hXb = C {node = ("h", 2) ; children = [ V "x" ; b]} ;;
+let t1 = C {node = ("f", 2) ; children = [haX ; hXb]} ;;
+let hab = C {node = ("h", 2) ; children = [a ; b]} ;;
+let hZX = C {node = ("h", 2) ; children = [V "z" ; V "x" ]} ;;
+let t2 = C {node = ("f", 2) ; children = [hab ; hZX]} ;;
+let t3 = C {node = ("f", 2); children = [haX; hXb]}
 let s2 = C {node = ("b", 0); children = []};;
 let s1 = C {node = ("a", 2); children = [s2; V "x"]};;
 let p1 = C {node = ("a", 2); children = [V "x"; V "z"]};;
@@ -129,8 +160,10 @@ let p1 = C {node = ("a", 2); children = [V "x"; V "z"]};;
 (* print_tree 0 (compose_subst [(t3, t4)] [(t4, t1)] t);; *)
 (* print_list([("x", t3); ("y", t4)]);; *)
 (* print_list([("a", t1); ("b", t2)]);; *)
-(* print_list(compose_subst [("x", t3); ("y", t4)] [("a", t2); ("b", t1)]);; *)
-(* print_list (mgu t1 t2);; *)
+(* print_list(compose [("x", (V "z"))] [("z", a)]);; *)
+(* print_list (mgu t2 t3);; *)
+(* print_list (mgu hXb hZX);; *)
+(* print_list (mgu (V "x") (V "z")) *)
 (* print_list (mgu (mirror t) (mirror t5));; *)
 (* print_string (string_of_bool (list_equal (mgu t5 t) (mgu (mirror t) (mirror t5))));; *)
 
