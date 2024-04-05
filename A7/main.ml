90611124e911eb1f597c4a09e50fb92c9d7160b7
rogerthat1729 <cs1221603@iitd.ac.in>
2024-04-05 22:01:07 +0530

    handled add, sub etc. inside krivine function

--- A7/main.ml
+++ A7/main.ml
@@ -29,130 +29,124 @@ let get_focus c =
   match c with
   | c, _ -> c
 
-let rec krivine (focus:clos) (stack:(clos list)) =
+let rec krivine (focus:clos) (stack:(clos list)) : (clos * (clos list)) =
 match focus with 
 | Clos(V x, g) -> krivine (try List.assoc x g with Not_found -> failwith "Variable not in gamma") stack
 | Clos(Abs(x, e), g) ->
     (
       match stack with
       | cl::s -> krivine (Clos(e, (x,cl)::g)) s
-      | _ -> failwith "Argument not on stack"
+      | [] -> Clos(Abs(x, e), g), stack
     )
 | Clos(App(e1, e2), g) -> krivine (Clos(e1, g)) (Clos(e2, g)::stack)
+| Clos(Add(e1, e2), g) -> 
+  (
+    match get_focus (krivine (Clos(e1, g)) stack), get_focus (krivine (Clos(e2, g)) stack) with
+    | Clos(N n1, _), Clos(N n2, _) -> Clos(N (n1+n2), g), stack
+    | _ -> failwith "Type error"
+  )
+| Clos(Sub(e1, e2), g) -> 
+  (
+    match get_focus (krivine (Clos(e1, g)) stack), get_focus (krivine (Clos(e2, g)) stack) with
+    | Clos(N n1, _), Clos(N n2, _) -> Clos(N (n1-n2), g), stack
+    | _ -> failwith "Type error"
+  )
+| Clos(Mul(e1, e2), g) -> 
+  (
+    match get_focus (krivine (Clos(e1, g)) stack), get_focus (krivine (Clos(e2, g)) stack) with
+    | Clos(N n1, _), Clos(N n2, _) -> Clos(N (n1*n2), g), stack
+    | _ -> failwith "Type error"
+  )
+| Clos(Div(e1, e2), g) -> 
+  (
+    match get_focus (krivine (Clos(e1, g)) stack), get_focus (krivine (Clos(e2, g)) stack) with
+    | Clos(N n1, _), Clos(N n2, _) -> if n2=0 then failwith "Division by Zero" else Clos(N (n1/n2), g), stack
+    | _ -> failwith "Type error"
+  )
+| Clos(Eq(e1, e2), g) -> 
+  (
+    match get_focus (krivine (Clos(e1, g)) stack), get_focus (krivine (Clos(e2, g)) stack) with
+    | Clos(N n1, _), Clos(N n2, _) -> Clos(B (n1=n2), g), stack
+    | _ -> failwith "Type error"
+  )
+| Clos(Gt(e1, e2), g) -> 
+  (
+    match get_focus (krivine (Clos(e1, g)) stack), get_focus (krivine (Clos(e2, g)) stack) with
+    | Clos(N n1, _), Clos(N n2, _) -> Clos(B (n1>n2), g), stack
+    | _ -> failwith "Type error"
+  )
+| Clos(Lt(e1, e2), g) -> 
+  (
+    match get_focus (krivine (Clos(e1, g)) stack), get_focus (krivine (Clos(e2, g)) stack) with
+    | Clos(N n1, _), Clos(N n2, _) -> Clos(B (n1<n2), g), stack
+    | _ -> failwith "Type error"
+  )
+| Clos(And(e1, e2), g) -> 
+  (
+    match get_focus (krivine (Clos(e1, g)) stack), get_focus (krivine (Clos(e2, g)) stack) with
+    | Clos(B b1, _), Clos(B b2, _) -> Clos(B (b1 && b2), g), stack
+    | _ -> failwith "Type error"
+  )
+| Clos(Or(e1, e2), g) -> 
+  (
+    match get_focus (krivine (Clos(e1, g)) stack), get_focus (krivine (Clos(e2, g)) stack) with
+    | Clos(B b1, _), Clos(B b2, _) -> Clos(B (b1 || b2), g), stack
+    | _ -> failwith "Type error"
+  )
+| Clos(Not(e), g) -> 
+  (
+    match get_focus (krivine (Clos(e, g)) stack) with
+    | Clos(B b, _) -> Clos(B (not b), g), stack
+    | _ -> failwith "Type error"
+  )
+| Clos(If(e0, e1, e2), g) -> 
+  (
+    match get_focus (krivine (Clos(e0, g)) stack) with
+    | Clos(B true, _) -> krivine (Clos(e1, g)) stack
+    | Clos(B false, _) -> krivine (Clos(e2, g)) stack
+    | _ -> failwith "Type error"
+  )
+| Clos(Tuple(l, n), g) -> 
+  (
+    let rec eval_tuple (l:exp list) (g:table) (stack:(clos list)) :exp list =
+      match l with
+      | [] -> []
+      | e::l1 -> 
+        (
+          match get_focus (krivine (Clos(e, g)) stack) with
+          | Clos(e', _) -> e'::(eval_tuple l1 g stack)
+        )
+    in
+    Clos(Tuple((eval_tuple l g stack), n), g), stack
+  )
+| Clos(Proj(e, n), g) -> 
+  (
+    match get_focus (krivine (Clos(e, g)) stack) with
+    | Clos(Tuple(l, m), _) -> 
+        (
+          if n <= 0 || n > m then
+            failwith "Invalid projection index"
+          else Clos(List.nth l (n-1), g), stack
+        )
+    | _ -> failwith "Type error"
+  )
+| Clos(Case(e, l), g) ->
+  (
+    let l1 = List.map (fun (a, b) -> (match get_focus(krivine (Clos(a, g)) stack) with | Clos(e', _) -> e', b)) l in
+    match get_focus (krivine (Clos(e, g)) stack) with
+    | Clos(e', _) -> try krivine (Clos(List.assoc e' l1, g)) stack with Not_found -> failwith "Invalid expression to be matched"
+    | _ -> failwith "Type error"
+  )
 | _ -> focus, stack
 ;;
 
 let rec unpack (focus:clos) : exp =
-match focus with
-| Clos(N n, g) -> N n
-| Clos(B b, g) -> B b
-| Clos(V x, g) -> (try unpack (List.assoc x g) with Not_found -> failwith "Variable not in gamma")
-| Clos(Add(e1, e2), g) -> Add((unpack (Clos(e1, g))), (unpack (Clos(e2, g))))
-| Clos(Sub(e1, e2), g) -> Sub((unpack (Clos(e1, g))), (unpack (Clos(e2, g))))
-| Clos(Mul(e1, e2), g) -> Mul((unpack (Clos(e1, g))), (unpack (Clos(e2, g))))
-| Clos(Div(e1, e2), g) -> Div((unpack (Clos(e1, g))), (unpack (Clos(e2, g))))
-| Clos(Eq(e1, e2), g) -> Eq((unpack (Clos(e1, g))), (unpack (Clos(e2, g))))
-| Clos(Gt(e1, e2), g) -> Gt((unpack (Clos(e1, g))), (unpack (Clos(e2, g))))
-| Clos(Lt(e1, e2), g) -> Lt((unpack (Clos(e1, g))), (unpack (Clos(e2, g))))
-| Clos(And(e1, e2), g) -> And((unpack (Clos(e1, g))), (unpack (Clos(e2, g))))
-| Clos(Or(e1, e2), g) -> Or((unpack (Clos(e1, g))), (unpack (Clos(e2, g))))
-| Clos(Not(e), g) -> Not((unpack (Clos(e, g))))
-| Clos(If(e0, e1, e2), g) -> If((unpack (Clos(e0, g))), (unpack (Clos(e1, g))), (unpack (Clos(e2, g))))
-| Clos(Tuple(l, n), g) -> Tuple((List.map (fun e -> unpack (Clos(e, g))) l), n)
-| Clos(Proj(e, n), g) -> Proj(unpack (Clos(e, g)), n)
-| Clos(Case(e, l), g) -> Case(unpack (Clos(e, g)), (List.map (fun (a, b) -> (unpack (Clos(a, g)), unpack (Clos(b, g)))) l))
-| _ -> failwith "invalid closure to unpack"
+  match focus with
+  | Clos(e, g) -> e
 ;;
 
-let rec eval (e:exp) : exp =
-match e with
-| N n -> N n
-| B b -> B b
-| Add(e1, e2) -> 
-    (
-      match eval(e1), eval(e2) with
-      | N n1, N n2 -> N (n1+n2)
-      | _ -> failwith "Type error"
-    )
-| Sub(e1, e2) -> 
-    (
-      match eval(e1), eval(e2) with
-      | N n1, N n2 -> N (n1-n2)
-      | _ -> failwith "Type error"
-    )
-| Mul(e1, e2) ->
-    (
-      match eval(e1), eval(e2) with
-      | N n1, N n2 -> N (n1*n2)
-      | _ -> failwith "Type error"
-    )
-| Div(e1, e2) ->
-    (
-      match eval(e1), eval(e2) with
-      | N n1, N n2 -> if n2=0 then failwith "Division by Zero" else N (n1/n2)
-      | _ -> failwith "Type error"
-    )
-| Eq(e1, e2) ->
-    (
-      match eval(e1), eval(e2) with
-      | N n1, N n2 -> B (n1=n2)
-      | _ -> failwith "Type error"
-    )
-| Gt(e1, e2) ->
-    (
-      match eval(e1), eval(e2) with
-      | N n1, N n2 -> B (n1>n2)
-      | _ -> failwith "Type error"
-    )
-| Lt(e1, e2) ->
-    (
-      match eval(e1), eval(e2) with
-      | N n1, N n2 -> B (n1<n2)
-      | _ -> failwith "Type error"
-    )
-| And(e1, e2) ->
-    (
-      match eval(e1), eval(e2) with
-      | B b1, B b2 -> B (b1 && b2)
-      | _ -> failwith "Type error"
-    )
-| Or(e1, e2) ->
-    (
-      match eval(e1), eval(e2) with
-      | B b1, B b2 -> B (b1 || b2)
-      | _ -> failwith "Type error"
-    )
-| Not(e) ->
-    (
-      match eval(e) with
-      | B b -> B (not b)
-      | _ -> failwith "Type error"
-    )
-| If(e0, e1, e2) ->
-    (
-      match eval(e0) with
-      | B true -> eval(e1)
-      | B false -> eval(e2)
-      | _ -> failwith "Type error"
-    )
-| Tuple(l, n) -> Tuple ((List.map eval l), n)
-| Proj(e, n) ->
-    (
-      match e with
-      | Tuple(l, m) ->
-          (
-            if n <= 0 || n > m then
-              failwith "Invalid projection index"
-            else eval (List.nth l (n-1))
-          )
-      | _ -> failwith "Type error"
-    )
-| Case(e, l) -> eval(try List.assoc (eval e) l with Not_found -> failwith "Invalid expression to be matched")
-| _ -> failwith "Invalid exp"
+let cbn (e:exp) :exp = unpack (get_focus (krivine (formclos e) []))
 ;;
-
-let cbn (e:exp) :exp = eval (unpack (get_focus (krivine (formclos e) [])))
-;; 
 
 let rec print_spaces (n:int) =
   if n > 0 then (print_string " "; print_spaces (n-1))
@@ -202,29 +196,21 @@ let rec print_clos (indent:int) (c:clos) =
 ;;
 
 (* let exp1 = App(Abs("x", N 3), Div(N 2, N 0));;
-print_clos 0 (formclos exp1);; 
-print_clos 0 (get_focus (krivine (formclos exp1) []));;
 print_exp 0 (cbn exp1) ;; *)
 
 (* let exp2 = Proj(Tuple([Tuple([], 0); (N 1)], 2), 1);;
-print_clos 0 (formclos exp2);;
-print_clos 0 (get_focus (krivine (formclos exp2) []));;
 print_exp 0 (cbn exp2) ;; *)
 
 (* let exp3 = App(App(Abs("x", Abs("y", (Add(V "y", N 2)))), Div(N 4, N 0)), N 5);;
-print_clos 0 (formclos exp3);;
-print_clos 0 (get_focus (krivine (formclos exp3) []));;
 print_exp 0 (cbn exp3) ;; *)
 
 (* let exp4 = App(Abs("x", Mul(V "x", N 0)), Div(N 7, N 0));;
-print_clos 0 (formclos exp4);;
-print_clos 0 (get_focus (krivine (formclos exp4) []));;
 print_exp 0 (cbn exp4) ;; *)
 
 (* let exp5 = Abs("x", Mul(V "x", N 0));;
-print_clos 0 (formclos exp5);;
-print_clos 0 (get_focus (krivine (formclos exp5) []));;
 print_exp 0 (cbn exp5) ;; *)
 
 (* let exp6 = V "x";;
 print_clos 0 (get_focus (krivine (Clos(exp6, [("x", Clos(App(Abs("y", N 3), Div(N 2, N 0)), []))])) []));; *)
+
+
