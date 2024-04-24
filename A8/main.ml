f533cbc0e4c813dbf4f13cb141fd1b86615fa917
rogerthat1729 <cs1221603@iitd.ac.in>
2024-04-24 15:35:27 +0530

    lists working :)

--- A8/main.ml
+++ A8/main.ml
@@ -1,81 +1,5 @@
 open Printf
 open Types
-
-(*
-let rec print_spaces n = 
-  match n with
-  | 0 -> ()
-  | _ -> print_string "  "; print_spaces (n - 1)
-      
-let rec print_term term indent =
-  match term with
-  | Program p -> print_spaces indent; print_string "Program\n"; print_term p (indent+2)
-  | Clause c -> print_spaces indent; print_string "Clause\n"; print_term c (indent+2)
-  | Fact a -> print_spaces indent; print_string "Fact\n"; print_term a (indent+2)
-  | Rule (a, b) -> print_spaces indent; print_string "Rule\n"; print_term a (indent+2); print_term b (indent+2)
-  | Termlist l -> List.iter (fun t -> print_term t indent) l
-  | Head h -> print_spaces indent; print_string "Head\n"; print_term h (indent+2)
-  | Body b -> print_spaces indent; print_string "Body\n"; print_term b (indent+2)
-  | Goal g -> print_spaces indent; print_string "Goal\n"; print_term g (indent+2)
-  | Int i -> print_spaces indent; print_string "Int: "; print_int i; print_string "\n"
-  | String s -> print_spaces indent; print_string "String: "; print_string s; print_string "\n"
-  | Variable v -> print_spaces indent; print_string "Variable: "; print_string v; print_string "\n"
-  | Keyword k -> print_spaces indent; print_string "Keyword: "; print_string k; print_string "\n"
-  | Atom a -> print_spaces indent; print_string "Atom: "; print_string a; print_string "\n"
-  | Bool b -> print_spaces indent; print_string "Bool: "; print_string (string_of_bool b); print_string "\n"
-  | Op (a, op, b) ->
-      print_spaces indent;
-      print_string "Operator Statement:\n";
-      print_spaces (indent+2);
-      print_string "Variable: ";
-      print_string a; 
-      print_string "\n";
-      print_spaces (indent+2);          
-      print_string "Operator: "; 
-      print_string op; 
-      print_string "\n";   
-      print_spaces (indent+2);       
-      print_string "Term:\n"; 
-      print_term b (indent+4);
-  | Atomicformula (atom, terms) -> 
-    print_spaces indent;
-    print_string "Atomic Formula:\n";            
-    print_spaces (indent+2);
-    print_string "Functor: "; 
-    print_string atom; 
-    print_string "\n";           
-    print_spaces (indent+2);
-    print_string "Argument Terms:\n"; 
-    print_term terms (indent+4)
-  | Funct (atom, terms) ->
-      print_spaces indent;
-      print_string "Function:\n";            
-      print_spaces (indent+2);
-      print_string "Function Name: "; 
-      print_string atom; 
-      print_string "\n";            
-      print_spaces (indent+2);
-      print_string "Arguments:\n"; 
-      print_term terms (indent+4)
-  ;;  
-
-let change_goal_tree (t:termtype) :termtype list =
-  match t with
-  | Program (Termlist l) -> l
-  | _ -> failwith "Enter a valid goal"
-;;
-
-let rec give_vars t =
-  match t with
-  | Int _ | String _ | Atom _ | Keyword _ | Bool _ -> []
-  | Variable v -> [v]
-  | Program p | Clause p | Fact p | Head p | Body p | Goal p -> give_vars p
-  | Rule (a, b) -> (give_vars a) @ (give_vars b)
-  | Termlist l -> List.fold_left (fun acc x -> acc @ (give_vars x)) [] l
-  | Op (a, _, b) -> a :: (give_vars b)
-  | Atomicformula (_, b) | Funct (_, b) -> give_vars b
-*)
-
 let rec print_term (t:termtype) = 
   match t with 
   | Vector (s, l) -> 
@@ -84,39 +8,28 @@ let rec print_term (t:termtype) =
       | "_LST" -> print_string "["; (List.iter 
       (fun t -> match t with 
       | Variable x -> if String.contains x '@' then print_string "_" else print_string x
+      | Nil -> ()
       | _ -> print_term t; print_string "|"
       ) l); print_string "]";
-      | _ -> print_string "Atomic Formula:\n"; print_endline s; print_string "Arguments:\n";
-      (List.iter (print_term) l) 
+      | _ -> if List.length l > 0 then 
+        (print_string "Atomic Formula:\n"; print_endline s; print_string "Arguments:\n"; (List.iter (fun ter -> print_term ter; print_string ",") l); print_endline ""; )
+            else print_string "Atom "; print_string s;
     )
   | Variable x -> print_string "Var "; print_string x
-  | Atom a -> print_string "Atom "; print_string a;
-  | Int x -> print_string "Int "; print_string (string_of_int x);
-  | String x -> print_string "String "; print_string x;
-  | Bool x -> print_string "Bool "; print_string (string_of_bool x);
-  | Nil -> print_endline "Empty"
-  | Fail -> print_endline "Fail"
+  | Atom a -> print_string a;
+  | Int x -> print_string (string_of_int x);
+  | String x -> print_char '\"';print_string x; print_char '\"';
+  | Bool x -> print_string (string_of_bool x);
+  | Nil -> print_string "[]"
+  | Fail -> print_string "Fail"
   | _ -> ()
 ;;
 
 let print_table table =
   print_string "\nTable:\n";
-  List.iter (fun (x, t) -> print_string x; print_string " = "; print_term t) table;
+  List.iter (fun (x, t) -> (print_string x; print_string " = "; print_term t; print_string ", ";)) table;
   print_endline ""
 ;;
-
-(* let rec check_unifiable (t1:termtype) (t2:termtype) =
-  match t1, t2 with
-  | Int x, Int y -> if x = y then true else false
-  | String x, String y -> if x = y then true else false
-  | Atom x, Atom y -> if x = y then true else false
-  | Keyword x, Keyword y -> if x = y then true else false
-  | Bool x, Bool y -> if x = y then true else false
-  | Variable x, Variable y -> true
-  | Variable x, t | t, Variable x -> true
-  | Atomicformula (a1, b1), Atomicformula (a2, b2) -> 
-    (check_unifiable a1 a2) && (List.fold_left (&&) true (List.map2 check_unifiable b1 b2))
-  | _ -> false *)
 
 let rec union l1 l2 =
   match l1 with
@@ -228,34 +141,9 @@ let convert_goal_to_list (g:termtype) =
   | _ -> failwith "Not a goal"
   ;;
 
-(* let rec give_vars t =
-  match t with 
-  | Variable x -> [x]
-  | Atomicformula (Atom a, l) -> List.flatten (List.map give_vars l)
-  | List (hd, tl) -> (give_vars hd) @ (give_vars tl)
-  | _ -> [] *)
-
-(* let rec af_equal t1 t2 = 
-  match t1, t2 with
-  | Atomicformula (a1, l1), Atomicformula (a2, l2) ->
-    if a1 = a2 then List.fold_left (&&) true (List.map2 (fun x y -> af_equal x y) l1 l2)
-    else false
-  | List(hd1, tl1), List(hd2, tl2) -> (af_equal hd1 hd2) && (af_equal tl1 tl2)
-  | Int x, Int y -> if x = y then true else false
-  | String x, String y -> if x = y then true else false
-  | Atom x, Atom y -> if x = y then true else false
-  | Bool x, Bool y -> if x = y then true else false
-  | _, _ -> false *)
-
-(* let check_var_in_af x t = 
-  let vars = give_vars t in
-  List.mem x vars *)
-
-(* let check_var_in_list x t =
-  let vars = give_vars t in
-  List.mem x vars *)
-
-let rec solve_goal s (goal:termtype list) (prog) :(bool * ((string * termtype) list)) =
+let rec solve_goal s (goal:termtype list) (prog) itr :(bool * ((string * termtype) list))  =
+  if itr < 1000 then
+    (
   match goal with
   | [] -> true, s
   | g :: gs -> 
@@ -276,7 +164,7 @@ let rec solve_goal s (goal:termtype list) (prog) :(bool * ((string * termtype) l
               let newsubs = compose s subs in
               (* print_table newsubs; *)
               (* print_int (List.length newgoal); print_endline ""; *)
-              let ans, finalsubs = solve_goal newsubs newgoal newprog in
+              let ans, finalsubs = solve_goal newsubs newgoal newprog (itr+1) in
               (* print_endline "Final sub";
               print_table finalsubs; *)
               if ans then true, finalsubs
@@ -288,6 +176,8 @@ let rec solve_goal s (goal:termtype list) (prog) :(bool * ((string * termtype) l
             in
         run_on_prog g prog
     )
+    )
+          else false, []
       ;;
 
 let apply_on_input buf = 
@@ -310,9 +200,9 @@ let print_ans (b:bool) s =
       let goal_vars = List.filter (fun (x, t) -> not (String.contains x '_')) s in
       (* print_table goal_vars; *)
       if List.length goal_vars = 0 then print_string "true.\n"
-      else List.iter (fun (x, t) -> print_string x; print_string " = "; print_term t) goal_vars
+      else print_table goal_vars
     )
-  | false -> print_endline "false"
+  | false -> print_endline "false.\n"
     ;;
 
 let main =
@@ -336,8 +226,10 @@ let main =
   (* unify_atomic_formulae (List.nth g 0) (fst (List.nth p 1)) *)
   (* unify_atomic_formulae (List.nth g 0) (fst (List.nth p 0)) *)
   (* solve_goal [] g p *)
-  let b,s = solve_goal [] g p in
+  let b,s = solve_goal [] g p 0 in
   print_ans b s
+  (* let tab = compose [("X", Atom "a"); ("_X", Atom "a"); ("_Y", Atom "a"); ("_Z", Atom "b")] [("___X", Atom "b"); ("___Y", Atom "a")] in *)
+  (* print_table tab; *)
   (* check_program (List.nth g 0) p *)
   (* check_unifiable (Atomicformula (Atom "mother", [Variable "X"; Variable "Y"])) (Atomicformula (Atom "mother", [Variable "A"; Variable "B"])) *)
   (* List.map (fun gl -> check_program gl p) g *)
